d2e5b5c552299528e422a213c220d17164334c1d
BrianBontempi <brian.bont54@gmail.com>
2024-04-03 21:46:54 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+  name: 'Boolfolio',
+  components: { AppHeader }
+};
+</script>
+
+<template>
+  <AppHeader />
+  <main class="container pt-4">
+    <RouterView />
+  </main>
+</template>
+
+<style scoped></style>
