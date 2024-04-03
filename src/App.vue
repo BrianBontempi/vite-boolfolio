8e22c909bd2ace79484fcc6c27bdd3a4a1187313
BrianBontempi <brian.bont54@gmail.com>
2024-04-03 21:53:13 +0200

    added AppHeader

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
+import axios from 'axios';
+const endpoint = 'http://localhost:8000/api/projects/';
 export default {
   name: 'Boolfolio',
-  components: { AppHeader }
+  components: { AppHeader },
+  data: () => ({ projects: [] }),
+  methods: {
+    fetchProjects() {
+      axios.get(endpoint).then(res => {
+        this.projects = res.data;
+      })
+    }
+  },
+  created() {
+    this.fetchProjects();
+  }
 };
 </script>
 
 <template>
   <AppHeader />
   <main class="container pt-4">
-    <RouterView />
+    <h1>Boolfolio</h1>
+
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        <h4 v-text="project.title"></h4>
+      </li>
+    </ul>
   </main>
 </template>
 
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,7 +8,7 @@ export default {
     <header>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <RouterLink class="navbar-brand" :to="{ name: 'home' }">Boolfolio</RouterLink>
+                <a class="navbar-brand" href="#">Boolfolio</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -16,10 +16,7 @@ export default {
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <RouterLink class="nav-link" :to="{ name: 'home' }">Home</RouterLink>
-                        </li>
-                        <li class="nav-item">
-                            <RouterLink class="nav-link" :to="{ name: 'contact-us' }">Contattaci</RouterLink>
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
                     </ul>
                 </div>
